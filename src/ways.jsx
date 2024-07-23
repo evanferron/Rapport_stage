b6c32c45c0f246ff5082071a9576606f731dbf6c
Evan FERRON (EURIAL) <evan.ferron@eurial.eu>
2024-07-23 13:31:30 +0200

    :sparkles: | initialize ways

--- src/ways.jsx
+++ src/ways.jsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
+import Entreprise from "./pages/entreprise/entreprise";
+import Articles from "./pages/articles/articles";
+import Article from "./pages/article/article";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
+import Error from "./pages/error/error";
 
 function Ways() {
   return (
@@ -7,6 +13,12 @@ function Ways() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/entreprise" element={<Entreprise />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/article" element={<Article />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/error" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </>
